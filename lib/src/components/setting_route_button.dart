cb4de41ccb5ebc7febf16a6ba814e4445f3f22f7
RitsuMurakami <beinosuke01@gmail.com>
2024-02-10 14:09:25 +0900

    remove: card

--- lib/src/components/setting_route_button.dart
+++ lib/src/components/setting_route_button.dart
@@ -15,29 +15,28 @@ class SettingRouteButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child: Container(
-            padding: const EdgeInsets.all(10),
-            child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) => nextPage));
-                },
-                style: ElevatedButton.styleFrom(
-                    elevation: 2,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                child: SizedBox(
-                    width: 250,
-                    height: 110,
-                    child: Row(
-                      children: [
-                        icon,
-                        const SizedBox(width: 32),
-                        Text(title,
-                            style: const TextStyle(
-                                fontSize: 32, fontWeight: FontWeight.bold))
-                      ],
-                    )))));
+    return Container(
+        padding: const EdgeInsets.all(10),
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => nextPage));
+            },
+            style: ElevatedButton.styleFrom(
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10))),
+            child: SizedBox(
+                width: 250,
+                height: 110,
+                child: Row(
+                  children: [
+                    icon,
+                    const SizedBox(width: 32),
+                    Text(title,
+                        style: const TextStyle(
+                            fontSize: 32, fontWeight: FontWeight.bold))
+                  ],
+                ))));
   }
 }
