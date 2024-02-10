e4636426e9b537b848a64c37503ab9f97bc46081
RitsuMurakami <beinosuke01@gmail.com>
2024-02-10 15:32:16 +0900

    change: backpack_route_button  to simple_route_button

---
+++ lib/src/components/simple_route_button.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+// Backpack画面で一覧されるボタン
+class BackpackRouteButton extends StatelessWidget {
+  const BackpackRouteButton(
+      {super.key,
+      required this.title,
+      required this.icon,
+      //required this.onPressed});
+      required this.nextPage});
+
+  final String title;
+  final Icon icon;
+  final Widget nextPage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(10),
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => nextPage));
+            },
+            style: ElevatedButton.styleFrom(
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10))),
+            child: SizedBox(
+                width: 250,
+                height: 110,
+                child: Row(
+                  children: [
+                    icon,
+                    const SizedBox(width: 32),
+                    Text(title,
+                        style: const TextStyle(
+                            fontSize: 32, fontWeight: FontWeight.bold)),
+                    const Expanded(child: SizedBox()),
+                    const Icon(Icons.arrow_forward_ios),
+                  ],
+                ))));
+  }
+}
