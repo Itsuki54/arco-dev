39d7e8ab6f3b480865bf53ad634c693e9594736b
RitsuMurakami <beinosuke01@gmail.com>
2024-02-10 13:04:37 +0900

    add: party button

--- lib/src/pages/setting/party.dart
+++ lib/src/pages/setting/party.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // components
 import '../../components/setting_child_appbar.dart';
+import '../../components/party_route_button.dart';
 
 class PartyPage extends StatefulWidget {
   const PartyPage({super.key});
@@ -12,15 +13,27 @@ class PartyPage extends StatefulWidget {
 class _PartyPage extends State<PartyPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: SettingChildAppBar(
+    return Scaffold(
+      appBar: const SettingChildAppBar(
           icon: Icon(
             Icons.assignment_ind,
-            size: 45,
+            size: 42,
             color: Colors.black,
           ),
           title: "Party"),
-      body: Center(),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 32),
+            PartyRouteButton(
+                title: "キャラクター名",
+                icon: const Icon(Icons.person_outline, size: 45),
+                level: 45,
+                job: "警察官",
+                nextPage: Scaffold(appBar: AppBar()))
+          ],
+        ),
+      ),
     );
   }
 }
