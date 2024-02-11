09d4380cd07563a2dd607556a372f622f58ed449
RitsuMurakami <beinosuke01@gmail.com>
2024-02-11 11:56:35 +0900

    add: character table

--- lib/src/pages/backpack/members.dart
+++ lib/src/pages/backpack/members.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 // components
 import '../../components/child_appbar.dart';
+import '../../components/backpack_content_chip.dart';
 
 class MembersPage extends StatefulWidget {
   const MembersPage({super.key});
@@ -12,15 +13,29 @@ class MembersPage extends StatefulWidget {
 class _MemberPage extends State<MembersPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: ChildAppBar(
+    return Scaffold(
+      appBar: const ChildAppBar(
           icon: Icon(
             Icons.group,
             size: 45,
             color: Colors.black,
           ),
           title: "Members"),
-      body: Center(),
+      body: SingleChildScrollView(
+          child: Center(
+        child: Wrap(
+          spacing: 4,
+          runSpacing: 8,
+          children: [
+            for (int i = 0; i < 32; i++)
+              BackpackContentChip(
+                  name: "キャラ名 $i",
+                  level: 32,
+                  icon: const Icon(Icons.person, size: 45, color: Colors.white),
+                  color: Colors.red.shade300),
+          ],
+        ),
+      )),
     );
   }
 }
