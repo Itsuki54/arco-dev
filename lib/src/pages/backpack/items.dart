bd9f1be4d6f19bc7bb903c72252c332b00233fd2
RitsuMurakami <beinosuke01@gmail.com>
2024-02-11 12:15:59 +0900

    add: item, weapon

--- lib/src/pages/backpack/items.dart
+++ lib/src/pages/backpack/items.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 // components
 import '../../components/child_appbar.dart';
+import '../../components/backpack_content_chip.dart';
 
 class ItemsPage extends StatefulWidget {
   const ItemsPage({super.key});
@@ -12,15 +13,29 @@ class ItemsPage extends StatefulWidget {
 class _ToolPage extends State<ItemsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: ChildAppBar(
-          icon: Icon(
-            Icons.home_repair_service,
-            size: 45,
-            color: Colors.black,
+    return Scaffold(
+        appBar: const ChildAppBar(
+            icon: Icon(
+              Icons.home_repair_service,
+              size: 45,
+              color: Colors.black,
+            ),
+            title: "Items"),
+        body: SingleChildScrollView(
+            child: Center(
+          child: Wrap(
+            spacing: 4,
+            runSpacing: 8,
+            children: [
+              for (int i = 0; i < 32; i++)
+                BackpackContentChip(
+                    name: "アイテム名 $i",
+                    level: 32,
+                    icon: const Icon(Icons.category,
+                        size: 45, color: Colors.white),
+                    color: Colors.green.shade800),
+            ],
           ),
-          title: "Items"),
-      body: Center(),
-    );
+        )));
   }
 }
--- lib/src/pages/backpack/weapons.dart
+++ lib/src/pages/backpack/weapons.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import "package:flutter_svg/flutter_svg.dart";
 // components
 import '../../components/child_appbar.dart';
+import '../../components/backpack_content_chip.dart';
 
 class WeaponsPage extends StatefulWidget {
   const WeaponsPage({super.key});
@@ -11,16 +12,31 @@ class WeaponsPage extends StatefulWidget {
 }
 
 class _WeaponsPage extends State<WeaponsPage> {
+  SvgPicture swordsIcon = SvgPicture.asset("assets/images/swords.svg",
+      width: 42, height: 42, theme: const SvgTheme(currentColor: Colors.black));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ChildAppBar(
-          icon: SvgPicture.asset("assets/images/swords.svg",
-              width: 42,
-              height: 42,
-              theme: const SvgTheme(currentColor: Colors.black)),
-          title: "Wepons"),
-      body: Center(),
-    );
+        appBar: ChildAppBar(icon: swordsIcon, title: "Wepons"),
+        body: SingleChildScrollView(
+            child: Center(
+          child: Wrap(
+            spacing: 4,
+            runSpacing: 8,
+            children: [
+              for (int i = 0; i < 32; i++)
+                BackpackContentChip(
+                    name: "武器名 $i",
+                    level: 32,
+                    icon: SvgPicture.asset("assets/images/swords.svg",
+                        width: 42,
+                        height: 42,
+                        colorFilter: const ColorFilter.mode(
+                            Colors.white, BlendMode.srcIn)),
+                    color: Colors.blue.shade800),
+            ],
+          ),
+        )));
   }
 }
