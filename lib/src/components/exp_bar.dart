478bfc61ad3f7fd13a22c7f16aabe74740870962
RitsuMurakami <beinosuke01@gmail.com>
2024-02-12 21:23:00 +0900

    fix: constructor

--- lib/src/components/exp_bar.dart
+++ lib/src/components/exp_bar.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ExpBar extends StatefulWidget {
-  ExpBar({
+  const ExpBar({
     super.key,
-    required this.expValue,
     required this.width,
     required this.height,
     required this.color,
+    this.expValue = 0.1,
   });
 
   final double width;
   final double height;
   final Color color;
-  double expValue;
+  final double expValue;
 
   @override
   State<ExpBar> createState() => _ExpBar();
 }
 
 class _ExpBar extends State<ExpBar> {
+  late double expValue = widget.expValue;
+
   // 値の更新
   void update_value(double value) {
     setState(() {
-      widget.expValue = value;
+      expValue = value;
     });
   }
 
@@ -37,7 +39,7 @@ class _ExpBar extends State<ExpBar> {
             backgroundColor: Colors.grey.shade300,
             valueColor: AlwaysStoppedAnimation<Color>(widget.color),
             minHeight: 30,
-            value: widget.expValue,
+            value: expValue,
           ),
         ));
   }
