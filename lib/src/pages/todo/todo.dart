43bb53fc96cf1f1e653503264ef6b78eab22726a
RitsuMurakami <beinosuke01@gmail.com>
2024-02-13 10:01:04 +0900

    add: quest dialog

--- lib/src/pages/todo/todo.dart
+++ lib/src/pages/todo/todo.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 // components
-import '../../components/done_button.dart';
+//import '../../components/done_button.dart';
 import '../../components/user_status.dart';
 import '../../components/quest_state_chip.dart';
+import '../../components/quest_dialog.dart';
 // structs
 import '../../structs/quest.dart';
 
@@ -18,26 +19,31 @@ class _ToDoPageState extends State<ToDoPage> {
     Quest(
         name: "失われた人工物",
         description: "現代都市で失われた魔法のアーティファクトを見つける。アーティファクトは古代の秘密を秘めている。	",
+        state: "未完了",
         point: 100,
         id: 1),
     Quest(
         name: "影の通り魔",
         description: "都市の裏通りで謎の暗殺者が現れ、街を恐怖に陥れている。彼の正体を突き止め、街の安全を確保する。	",
+        state: "完了",
         point: 120,
         id: 2),
     Quest(
         name: "魔法の暴走",
         description: "都市の中心で突如として発生した魔法の暴走を止めるため、その原因を調査して封じ込める。	",
+        state: "受取り",
         point: 100,
         id: 1),
     Quest(
         name: "迷子の精霊",
         description: "都市の公園で迷子の精霊が現れ、助けを求めている。彼女を安全な場所へ導くことが求められる。	",
+        state: "受取り",
         point: 100,
         id: 1),
     Quest(
         name: "闇のコンクラーヴ",
         description: "都市の地下に潜む闇の組織が暗躍している。その勢力を探り、彼らの野望を阻止するために立ち向かう。",
+        state: "未完了",
         point: 100,
         id: 1),
   ];
@@ -82,42 +88,33 @@ class _ToDoPageState extends State<ToDoPage> {
                         ),
                         onPressed: () {
                           showDialog(
-                            context: context,
-                            builder: (BuildContext context) => AlertDialog(
-                                title: Text(
-                                  quests[i].name,
-                                  style: const TextStyle(
-                                      fontSize: 26,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                content: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      const SizedBox(height: 24),
-                                      Image.asset(
-                                        'assets/images/appicon.png',
-                                        width: 150,
-                                      ),
-                                      const SizedBox(height: 24),
-                                      Text(quests[i].description,
-                                          style: const TextStyle(
-                                              fontSize: 16,
-                                              fontWeight: FontWeight.bold)),
-                                      const SizedBox(height: 24),
-                                    ])),
-                          );
+                              context: context,
+                              builder: (BuildContext context) =>
+                                  QuestDialog(quest: quests[i]));
                         },
                         child: Container(
                             padding: const EdgeInsets.all(2),
                             child: Row(
                               children: [
-                                Text(quests[i].name,
-                                    style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold)),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(quests[i].name,
+                                        style: const TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold)),
+                                    Text(
+                                      "${quests[i].point} point",
+                                      style: const TextStyle(
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
                                 const Expanded(child: SizedBox()),
                                 //DoneButton(),
-                                QuestStateChip(),
+                                QuestStateChip(state: quests[i].state),
                               ],
                             )),
                       ),
