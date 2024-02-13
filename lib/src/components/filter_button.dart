c936568877eea506f418f5160ca0e291a6629b4f
RitsuMurakami <beinosuke01@gmail.com>
2024-02-13 14:56:18 +0900

    fix: state navigation

--- lib/src/components/filter_button.dart
+++ lib/src/components/filter_button.dart
@@ -1,42 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class FilterButton extends StatefulWidget {
   const FilterButton({
     super.key,
     required this.text,
     required this.color,
+    required this.onPressed,
     this.state = false,
   });
 
   final bool state;
   final String text;
   final Color color;
+  final VoidCallback onPressed;
 
   @override
   State<FilterButton> createState() => _FilterButton();
 }
 
 class _FilterButton extends State<FilterButton> {
-  late bool state = widget.state;
-
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: const EdgeInsets.all(2),
         child: OutlinedButton(
           style: OutlinedButton.styleFrom(
-              backgroundColor: state ? widget.color : Colors.white,
+              backgroundColor: widget.state ? widget.color : Colors.white,
               side: BorderSide(width: 2.5, color: widget.color)),
-          onPressed: () {
-            setState(() {
-              state = !state;
-            });
-          },
+          onPressed: widget.onPressed,
           child: Text(widget.text,
               style: TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.bold,
-                  color: state ? Colors.white : widget.color)),
+                  color: widget.state ? Colors.white : widget.color)),
         ));
   }
 }
