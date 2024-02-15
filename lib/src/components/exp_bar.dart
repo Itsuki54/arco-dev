4a23bfea9c511c331f7da910a41439290e7e4c3b
RitsuMurakami <beinosuke01@gmail.com>
2024-02-15 16:58:36 +0900

    add: expbar

--- lib/src/components/exp_bar.dart
+++ lib/src/components/exp_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ExpBar extends StatefulWidget {
+class ExpBar extends StatelessWidget {
   const ExpBar({
     super.key,
     required this.width,
@@ -15,29 +15,15 @@ class ExpBar extends StatefulWidget {
   final double expValue;
 
   @override
-  State<ExpBar> createState() => _ExpBar();
-}
-
-class _ExpBar extends State<ExpBar> {
-  late double expValue = widget.expValue;
-
-  // 値の更新
-  void update_value(double value) {
-    setState(() {
-      expValue = value;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: widget.width,
-        height: widget.height,
+        width: width,
+        height: height,
         child: ClipRRect(
           borderRadius: const BorderRadius.all(Radius.circular(30)),
           child: LinearProgressIndicator(
             backgroundColor: Colors.grey.shade300,
-            valueColor: AlwaysStoppedAnimation<Color>(widget.color),
+            valueColor: AlwaysStoppedAnimation<Color>(color),
             minHeight: 30,
             value: expValue,
           ),
