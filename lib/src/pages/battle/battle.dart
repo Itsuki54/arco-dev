443cda3af8af92142cd50d1c3179bcd73cb5a98a
RitsuMurakami <beinosuke01@gmail.com>
2024-02-17 14:48:51 +0900

    add: simple battle system

--- lib/src/pages/battle/battle.dart
+++ lib/src/pages/battle/battle.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import "package:flutter_svg/flutter_svg.dart";
 import 'package:audioplayers/audioplayers.dart';
 // components
@@ -54,6 +55,7 @@ class Enemy extends StatelessWidget {
 
 class BattleCharacter {
   BattleCharacter({
+    required this.name,
     required this.fullHP,
     this.attribute = "nemo",
     this.power = 1,
@@ -64,8 +66,9 @@ class BattleCharacter {
     crtHP = fullHP;
   }
 
-  late int crtHP;
+  final String name;
   final int fullHP;
+  late int crtHP;
   final String attribute;
   final int power;
   final int defense;
@@ -73,7 +76,10 @@ class BattleCharacter {
   final double helth;
 }
 
-class Battle {}
+class Battle {
+  List<BattleCharacter> party = [BattleCharacter(name: "Player", fullHP: 100)];
+  List<BattleCharacter> enemies = [BattleCharacter(name: "Enemy", fullHP: 10)];
+}
 
 class BattlePage extends StatefulWidget {
   const BattlePage({super.key});
@@ -93,6 +99,12 @@ class _BattlePage extends State<BattlePage> {
 
   late int enemyCrtHp = enemyFullHp;
   late int playerCrtHp = playerFullHp ~/ 2;
+
+  // テスト
+  BattleCharacter invader = BattleCharacter(name: "Invader", fullHP: 30);
+  BattleCharacter player = BattleCharacter(name: "player", fullHP: 30);
+  String turn = "party";
+  String battleMessage = "コマンド？";
 
   bool showShade = true;
 
@@ -114,9 +126,9 @@ class _BattlePage extends State<BattlePage> {
               children: [
                 for (int i = 0; i < 2; i++)
                   Enemy(
-                      enemyName: enemyName,
-                      enemyFullHp: enemyFullHp,
-                      enemyCrtHp: enemyCrtHp,
+                      enemyName: invader.name,
+                      enemyFullHp: invader.fullHP,
+                      enemyCrtHp: invader.crtHP,
                       image: SvgPicture.asset("assets/images/invader.svg",
                           width: 120,
                           colorFilter: const ColorFilter.mode(
@@ -124,17 +136,52 @@ class _BattlePage extends State<BattlePage> {
               ],
             ),
             const Expanded(child: SizedBox()),
+            Text(
+              battleMessage,
+              style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const Expanded(child: SizedBox()),
             Stack(
               children: [
                 BattleController(
-                    playerCrtHp: playerCrtHp,
-                    playerFullHp: playerFullHp,
-                    onAttack: () {},
+                    playerCrtHp: player.crtHP,
+                    playerFullHp: player.fullHP,
+                    onAttack: () {
+                      setState(() {
+                        turn = "enemy";
+                        battleMessage = "${player.name}の攻撃！";
+                      });
+                      Future.delayed(const Duration(seconds: 2), () {
+                        setState(() {
+                          invader.crtHP -= player.offensive;
+                          battleMessage =
+                              "${invader.name}に${player.offensive}のダメージ";
+                        });
+                        Future.delayed(const Duration(seconds: 2), () {
+                          setState(() {
+                            battleMessage = "${invader.name}の攻撃！";
+                          });
+                          Future.delayed(const Duration(seconds: 2), () {
+                            setState(() {
+                              player.crtHP -= invader.offensive;
+                              battleMessage =
+                                  "${player.name}に${invader.offensive}のダメージ";
+                              Future.delayed(const Duration(seconds: 2), () {
+                                setState(() {
+                                  battleMessage = "コマンド？";
+                                  turn = "party";
+                                });
+                              });
+                            });
+                          });
+                        });
+                      });
+                    },
                     onItem: () {},
                     onShield: () {},
                     onEscape: () {}),
                 Visibility(
-                    visible: !showShade,
+                    visible: turn != "party",
                     child: Card(
                       elevation: 0,
                       color: Colors.black.withOpacity(0.2),
@@ -142,7 +189,7 @@ class _BattlePage extends State<BattlePage> {
                           borderRadius: BorderRadius.circular(3)),
                       child: SizedBox(
                         width: MediaQuery.of(context).size.width,
-                        height: 260,
+                        height: 250,
                       ),
                     ))
               ],
