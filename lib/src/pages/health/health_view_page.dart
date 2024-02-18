565f2059e5c4935f783b669d46b294b55799bece
RitsuMurakami <beinosuke01@gmail.com>
2024-02-18 15:40:34 +0900

    add: health view

--- lib/src/pages/health/health_view_page.dart
+++ lib/src/pages/health/health_view_page.dart
@@ -1,5 +1,6 @@
 import 'package:arco_dev/src/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class HealthViewPage extends StatefulWidget {
   const HealthViewPage({super.key});
@@ -41,8 +42,11 @@ class HealthContentButton extends StatelessWidget {
                 children: [
                   const SizedBox(height: 8),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Container(
+                        width: 40,
+                        height: 40,
                         decoration: BoxDecoration(
                           color: color,
                           borderRadius: BorderRadius.circular(50),
@@ -52,7 +56,7 @@ class HealthContentButton extends StatelessWidget {
                       const SizedBox(width: 4),
                       Text(title,
                           style: TextStyle(
-                              fontSize: 22,
+                              fontSize: 20,
                               fontWeight: FontWeight.bold,
                               color: color))
                     ],
@@ -72,8 +76,83 @@ class HealthContentButton extends StatelessWidget {
 }
 
 class _HealthViewPage extends State<HealthViewPage> {
+  int touchedIndex = -1;
+
   @override
   Widget build(BuildContext context) {
+    List<PieChartSectionData> stepSections() {
+      return List.generate(2, (i) {
+        final isTouched = false;
+        final fontSize = isTouched ? 32.0 : 24.0;
+        final radius = isTouched ? 40.0 : 30.0;
+        switch (i) {
+          case 0:
+            return PieChartSectionData(
+              color: AppColors.orange,
+              value: 50,
+              title: "",
+              radius: radius,
+              titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            );
+          case 1:
+            return PieChartSectionData(
+              color: AppColors.lightOrange,
+              value: 10,
+              radius: radius,
+              title: "",
+              titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            );
+          default:
+            throw Error();
+        }
+      });
+    }
+
+    List<PieChartSectionData> calorieSections() {
+      return List.generate(2, (i) {
+        //final isTouched = i == touchedIndex;
+        final isTouched = false;
+        final fontSize = isTouched ? 32.0 : 24.0;
+        final radius = isTouched ? 40.0 : 30.0;
+        switch (i) {
+          case 0:
+            return PieChartSectionData(
+              color: AppColors.blue,
+              value: 50,
+              title: "",
+              radius: radius,
+              titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            );
+          case 1:
+            return PieChartSectionData(
+              color: AppColors.lightBlue,
+              value: 10,
+              radius: radius,
+              title: "",
+              titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            );
+          default:
+            throw Error();
+        }
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
           centerTitle: true,
@@ -84,6 +163,95 @@ class _HealthViewPage extends State<HealthViewPage> {
       body: Center(
           child: Column(
         children: [
+          const SizedBox(height: 18),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                  width: 300,
+                  height: 300,
+                  margin: const EdgeInsets.all(5),
+                  child: PieChart(
+                    PieChartData(
+                      startDegreeOffset: -90,
+                      pieTouchData: PieTouchData(
+                        touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                          setState(() {
+                            if (!event.isInterestedForInteractions ||
+                                pieTouchResponse == null ||
+                                pieTouchResponse.touchedSection == null) {
+                              touchedIndex = -1;
+                              return;
+                            }
+                            touchedIndex = pieTouchResponse
+                                .touchedSection!.touchedSectionIndex;
+                          });
+                        },
+                      ),
+                      borderData: FlBorderData(
+                        show: false,
+                      ),
+                      sectionsSpace: 0,
+                      centerSpaceRadius: 98,
+                      sections: calorieSections(),
+                    ),
+                  )),
+              Container(
+                  width: 300,
+                  height: 300,
+                  margin: const EdgeInsets.all(5),
+                  child: PieChart(
+                    PieChartData(
+                      startDegreeOffset: -90,
+                      pieTouchData: PieTouchData(
+                        touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                          setState(() {
+                            if (!event.isInterestedForInteractions ||
+                                pieTouchResponse == null ||
+                                pieTouchResponse.touchedSection == null) {
+                              touchedIndex = -1;
+                              return;
+                            }
+                            touchedIndex = pieTouchResponse
+                                .touchedSection!.touchedSectionIndex;
+                          });
+                        },
+                      ),
+                      borderData: FlBorderData(
+                        show: false,
+                      ),
+                      sectionsSpace: 0,
+                      centerSpaceRadius: 130,
+                      sections: stepSections(),
+                    ),
+                  )),
+              Column(
+                children: [
+                  Text("歩数",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.primary)),
+                  Text("7810歩",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.lightprimary)),
+                  Text("消費熱量",
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.primary)),
+                  Text("470kcal",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.lightprimary)),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(height: 32),
           Row(children: [
             HealthContentButton(
                 onPressed: () {},
