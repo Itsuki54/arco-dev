fb411602a60c5f7a19c68ead06469d5f0589ee9b
Itsuki54 <5shi.icyu@gmail.com>
2024-02-18 22:36:04 +0900

    add image button

--- lib/src/pages/settings/profile.dart
+++ lib/src/pages/settings/profile.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import '../../components/common/child_appbar.dart';
 // pages
 
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
@@ -12,6 +15,22 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  File _image = File("assets/images/sample_icon.png");
+  final picker = ImagePicker();
+
+  Future getImageFromGallery() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _image = File(pickedFile.path);
+      } else {
+        _image = File("default_image_path");
+      }
+    });
+  }
+
+  var isOn = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,16 +39,49 @@ class _ProfilePageState extends State<ProfilePage> {
       body: Center(
         child: Column(
           children: [
-            Image.asset('assets/images/sample_icon.png',
-                width: 100, height: 100, fit: BoxFit.cover),
+            Stack(alignment: Alignment.center, children: [
+              Container(
+                width: 120,
+                height: 120,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey.shade300,
+                ),
+              ),
+              Container(
+                width: 120.0,
+                height: 120.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        fit: BoxFit.fill, image: FileImage(_image))),
+              ),
+            ]),
+            ElevatedButton(
+              onPressed: () {
+                getImageFromGallery();
+              },
+              child: Text("画像を選択"),
+            ),
             const SizedBox(width: 10),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text("名前:"),
-                Text("ID:"),
-                Text("レベル:"),
-                Text("経験値:"),
+              children: [
+                Text("名前:", style: TextStyle(fontSize: 20)),
+                Text("ID:", style: TextStyle(fontSize: 20)),
+                Text("レベル:", style: TextStyle(fontSize: 20)),
+                Text("メールアドレス:", style: TextStyle(fontSize: 20)),
+                Switch(
+                  value: isOn,
+                  onChanged: (bool? value) {
+                    if (value != null) {
+                      setState(() {
+                        isOn = value;
+                        print("$isOn");
+                      });
+                    }
+                  },
+                ),
               ],
             ),
           ],
