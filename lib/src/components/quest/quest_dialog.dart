12c2520544003abef3950fd2ce7281e4eb88e4e0
RitsuMurakami <beinosuke01@gmail.com>
2024-02-23 11:47:24 +0900

    fix: id

--- lib/src/components/quest/quest_dialog.dart
+++ lib/src/components/quest/quest_dialog.dart
@@ -46,7 +46,7 @@ class QuestDialog extends StatelessWidget {
                     ? () {
                         db
                             .userQuestsCollection(uid)
-                            .update(quest.questId, {"state": "完了"});
+                            .update(quest.id, {"state": "完了"});
                         onChanged();
                         Navigator.of(context).pop(quest.point);
                       }
