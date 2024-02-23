15879df595c7c75f48c2b479f86b1e0f5d0d129b
RitsuMurakami <beinosuke01@gmail.com>
2024-02-23 11:15:14 +0900

    add: dialog that is used when profile is changed

--- lib/src/pages/settings/profile.dart
+++ lib/src/pages/settings/profile.dart
@@ -1,8 +1,9 @@
-import 'package:arco_dev/src/utils/colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 // components
 import '../../components/common/child_appbar.dart';
+import 'package:arco_dev/src/components/settings/authenticate_dialog.dart';
+import 'package:arco_dev/src/components/settings/change_name_dialog.dart';
 // pages
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
@@ -23,12 +24,14 @@ class _ProfilePageState extends State<ProfilePage> {
   File _image = File("assets/images/sample_icon.png");
   final picker = ImagePicker();
 
+  // firebase
   Database db = Database();
   Storage st = Storage();
+  final FirebaseAuth auth = FirebaseAuth.instance;
 
   // user info
-  String userName = "guest";
-  String email = "guest@guest";
+  String userName = "";
+  String email = "";
 
   Future<void> getImageFromGallery() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
@@ -43,8 +46,29 @@ class _ProfilePageState extends State<ProfilePage> {
     });
   }
 
-  Future<void> getEmailAddress() async {}
-  Future<void> getUserName() async {}
+  // 入力データをもとに、firebaseを更新する
+  Future<void> setUserProfile() async {
+    setState(() {
+      // ユーザ名と名前の変更
+      if (userName != "") {
+        db.usersCollection().update(widget.uid, {"name": userName});
+        showDialog<void>(
+            context: context,
+            builder: (_) {
+              return const ChangeNameDialog();
+            });
+      }
+      if (email != "") {
+        db.usersCollection().update(widget.uid, {"email": email});
+        auth.currentUser!.verifyBeforeUpdateEmail(email);
+        showDialog<void>(
+            context: context,
+            builder: (_) {
+              return const AuthenticateDialog();
+            });
+      }
+    });
+  }
 
   var levelPub = false;
 
@@ -54,161 +78,133 @@ class _ProfilePageState extends State<ProfilePage> {
         appBar: const ChildAppBar(
             title: "設定", icon: Icon(Icons.settings, size: 40)),
         body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(height: 32),
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Container(
-                      width: 200,
-                      height: 200,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.grey.shade300,
-                      ),
-                      child: const Icon(
-                        Icons.person,
-                        size: 150,
-                        color: Colors.black45,
-                      ),
-                    ),
-                    Container(
-                      width: 200.0,
-                      height: 200.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: FileImage(_image),
-                        ),
-                      ),
-                    ),
-                  ],
+            child: Center(
+                child: Column(children: [
+          const SizedBox(height: 32),
+          Stack(alignment: Alignment.center, children: [
+            Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.grey.shade300,
                 ),
-                const SizedBox(height: 32),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                    onPressed: () {
-                      getImageFromGallery();
-                    },
-                    child: const SizedBox(
-                        height: 52,
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "画像を選択",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 20),
-                              )
-                            ]))),
-                const SizedBox(height: 20),
-                Column(
-                  children: [
-                    Row(
+                child: const Icon(
+                  Icons.person,
+                  size: 150,
+                  color: Colors.black45,
+                )),
+            Container(
+                width: 200.0,
+                height: 200.0,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: FileImage(_image),
+                    )))
+          ]),
+          const SizedBox(height: 32),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+              onPressed: () {
+                getImageFromGallery();
+              },
+              child: const SizedBox(
+                  height: 52,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        const SizedBox(width: 30),
-                        Expanded(
-                          child: SizedBox(
-                            height: 50,
-                            child: TextField(
-                              onChanged: (value) {
-                                setState(() {
-                                  userName = value;
-                                });
-                              },
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                                labelText: '名前',
-                              ),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 30),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Column(
-                  children: [
-                    Row(
+                        Text(
+                          "画像を選択",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        )
+                      ]))),
+          const SizedBox(height: 20),
+          Column(children: [
+            Row(children: [
+              const SizedBox(width: 30),
+              Expanded(
+                  child: SizedBox(
+                      height: 50,
+                      child: TextField(
+                          onChanged: (value) {
+                            setState(() {
+                              userName = value;
+                            });
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            labelText: '名前',
+                          )))),
+              const SizedBox(width: 30),
+            ])
+          ]),
+          const SizedBox(height: 20),
+          Column(children: [
+            Row(children: [
+              const SizedBox(width: 30),
+              Expanded(
+                  child: SizedBox(
+                      height: 50,
+                      child: TextField(
+                          onChanged: (value) {
+                            setState(() {
+                              email = value;
+                              db
+                                  .usersCollection()
+                                  .update(widget.uid, {"email": email});
+                            });
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            labelText: 'メールアドレス',
+                          )))),
+              const SizedBox(width: 30)
+            ])
+          ]),
+          const SizedBox(height: 20),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const SizedBox(width: 30),
+            const Text("スコアの公開", style: TextStyle(fontSize: 20)),
+            const SizedBox(width: 12),
+            Switch(
+                value: levelPub,
+                onChanged: (bool? value) {
+                  if (value != null) {
+                    setState(() {
+                      levelPub = value;
+                    });
+                  }
+                })
+          ]),
+          const SizedBox(height: 120),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black, elevation: 3),
+              onPressed: () {
+                setUserProfile();
+              },
+              child: const SizedBox(
+                  height: 52,
+                  width: 200,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        const SizedBox(width: 30),
-                        Expanded(
-                          child: SizedBox(
-                            height: 50,
-                            child: TextField(
-                              onChanged: (value) {
-                                setState(() {
-                                  email = value;
-                                  db
-                                      .usersCollection()
-                                      .update(widget.uid, {"email": email});
-                                });
-                              },
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10)),
-                                labelText: 'メールアドレス',
-                              ),
-                            ),
+                        Text(
+                          "保存",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
                           ),
-                        ),
-                        const SizedBox(width: 30),
-                      ],
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(width: 30),
-                    const Text("スコアの公開", style: TextStyle(fontSize: 20)),
-                    const SizedBox(width: 12),
-                    Switch(
-                      value: levelPub,
-                      onChanged: (bool? value) {
-                        if (value != null) {
-                          setState(
-                            () {
-                              levelPub = value;
-                            },
-                          );
-                        }
-                      },
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 120),
-                ElevatedButton(
-                    style:
-                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                    onPressed: () {},
-                    child: const SizedBox(
-                        height: 52,
-                        width: 200,
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "保存",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              )
-                            ]))),
-              ],
-            ),
-          ),
-        ));
+                        )
+                      ])))
+        ]))));
   }
 }
